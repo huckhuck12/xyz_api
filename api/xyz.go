85b73926026131e7d49e719d7984b39a95d7ace2
邓德明 <dengdeming@aobi.com>
2025-12-03 16:46:23 +0800

    fix: vercel go handler export

--- api/xyz.go
+++ api/xyz.go
@@ -2,12 +2,23 @@ package api
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/ultrazg/xyz/service"
 )
 
-var Handler http.Handler
+var (
+	engineOnce sync.Once
+	engine     http.Handler
+)
 
-func init() {
-	Handler = service.NewEngine()
+func getHandler() http.Handler {
+	engineOnce.Do(func() {
+		engine = service.NewEngine()
+	})
+	return engine
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	getHandler().ServeHTTP(w, r)
 }
